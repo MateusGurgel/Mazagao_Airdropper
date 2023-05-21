07d7ad763ecf773f95003b9b6ea41e0010f35852
BuildTools <unconfigured@null.spigotmc.org>
2023-05-21 18:29:27 -0300

    Initial Commit

---
+++ src/main/java/me/waskjobe/mazagao_airdropper/Mazagao_AirDropper.java
@@ -0,0 +1,19 @@
+package me.waskjobe.mazagao_airdropper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Mazagao_AirDropper extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("Mazagão Dropper is enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        System.out.println("Mazagão Dropper is disabled");
+    }
+
+}
