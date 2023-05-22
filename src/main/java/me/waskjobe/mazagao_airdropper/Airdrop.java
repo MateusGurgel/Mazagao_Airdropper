ca38dbce033794b5b7a0b4209497131fc3b5fbd3
BuildTools <unconfigured@null.spigotmc.org>
2023-05-21 22:19:52 -0300

    Config Managment added

--- src/main/java/me/waskjobe/mazagao_airdropper/Airdrop.java
+++ src/main/java/me/waskjobe/mazagao_airdropper/Airdrop.java
@@ -1,2 +1,40 @@
-package me.waskjobe.mazagao_airdropper;public class Airdrop {
+package me.waskjobe.mazagao_airdropper;
+import org.bukkit.Location;
+import org.bukkit.World;
+
+public class Airdrop {
+
+    private static Location generateAirDropCords(World world){
+        Location location = new Location(world, 0, 0, 0);
+
+        final int randomX = 1;
+        final int randomY = 1;
+
+        location.setX(randomX);
+        location.setZ(randomY);
+
+        location.setY(world.getHighestBlockYAt(randomX, location.getBlockY()) + 1);
+
+        return location;
+    }
+
+    public static void call(World world){
+
+        //Get Location
+        Location location = generateAirDropCords(world);
+
+        //Get Loot
+
+
+        //hit that cord with thunders
+
+        //place the chest
+
+        //place the loot on the Chest
+
+        //Place the Chest
+
+        //Send Message
+    }
+
 }
