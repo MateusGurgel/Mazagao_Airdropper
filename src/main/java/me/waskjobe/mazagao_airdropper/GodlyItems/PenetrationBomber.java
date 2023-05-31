5c72cf72fd8f5d6afc6b89a1fb91e4683218de1f
BuildTools <unconfigured@null.spigotmc.org>
2023-05-31 12:11:20 -0300

    Penetration bomber added

---
+++ src/main/java/me/waskjobe/mazagao_airdropper/GodlyItems/PenetrationBomber.java
@@ -0,0 +1,48 @@
+package me.waskjobe.mazagao_airdropper.GodlyItems;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.minecart.ExplosiveMinecart;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class PenetrationBomber extends Bomber implements Listener {
+
+    public PenetrationBomber(Plugin plugin) {
+        super(plugin);
+        setItemName("Penetration Bomber Caller");
+        setKey("PenetrationBomberCaller");
+    }
+
+    @Override
+    void bombardment(Location location) {
+
+        World world = location.getWorld();
+        Integer amountOfTnt = 100;
+
+        BukkitRunnable bombardmentTask = new BukkitRunnable() {
+            private int count = 0;
+
+            @Override
+            public void run() {
+
+                count++;
+
+                if (count >= amountOfTnt) {
+                    cancel();
+                }
+
+                world.createExplosion(location, 10);
+                location.add(0,-1.3,0);
+
+
+            }
+        };
+
+        bombardmentTask.runTaskTimer(plugin, 0, 0);
+    }
+
+
+}
